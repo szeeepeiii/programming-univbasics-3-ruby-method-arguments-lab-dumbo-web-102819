adb953407bb4afd75e51fdfb95db73408163d9ae
Szepei <cheung.szepei@gmail.com>
2019-09-28 17:59:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,9 +3,12 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+name= Kristen
+print "Hi, my name is #{name}"
 
-
-
+name="Kristen"
+introduction_with_language=Ruby
+print="Hi, my name is #{name} and I am learning to program in #{introduction_with_language}"
 
 
 
